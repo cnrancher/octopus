aefcee6835f3b43a070c8cf9220675ea3b5837ff
Frank Mai <frank@rancher.com>
2020-02-27 14:43:17 +0800

    added unit test for pkg/util

---
+++ pkg/util/object/runtime_is_test.go
@@ -0,0 +1,64 @@
+package object
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	edgev1alpha1 "github.com/rancher/octopus/api/v1alpha1"
+)
+
+func TestIsNodeObject(t *testing.T) {
+	var testCases = []struct {
+		name   string
+		given  runtime.Object
+		expect bool
+	}{
+		{
+			name:   "node instance",
+			given:  &corev1.Node{},
+			expect: true,
+		},
+		{
+			name:   "none node instance",
+			given:  &edgev1alpha1.DeviceLink{},
+			expect: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		var ret = IsNodeObject(tc.given)
+		if ret != tc.expect {
+			t.Errorf("case %v: expected %s, got %s", tc.name, spew.Sprintf("%#v", tc.expect), spew.Sprintf("%#v", ret))
+		}
+	}
+}
+
+func TestIsCustomResourceDefinitionObject(t *testing.T) {
+	var testCases = []struct {
+		name   string
+		given  runtime.Object
+		expect bool
+	}{
+		{
+			name:   "CRD instance",
+			given:  &apiextensionsv1.CustomResourceDefinition{},
+			expect: true,
+		},
+		{
+			name:   "none CRD instance",
+			given:  &edgev1alpha1.DeviceLink{},
+			expect: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		var ret = IsCustomResourceDefinitionObject(tc.given)
+		if ret != tc.expect {
+			t.Errorf("case %v: expected %s, got %s", tc.name, spew.Sprintf("%#v", tc.expect), spew.Sprintf("%#v", ret))
+		}
+	}
+}
---
+++ pkg/util/object/runtime_to_test.go
@@ -0,0 +1,53 @@
+package object
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	edgev1alpha1 "github.com/rancher/octopus/api/v1alpha1"
+)
+
+func TestToDeviceLinkObject(t *testing.T) {
+	var testCases = []struct {
+		name   string
+		given  runtime.Object
+		expect *edgev1alpha1.DeviceLink
+	}{
+		{
+			name: "DeviceLink instance",
+			given: &edgev1alpha1.DeviceLink{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "default",
+					Name:      "test",
+				},
+			},
+			expect: &edgev1alpha1.DeviceLink{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "default",
+					Name:      "test",
+				},
+			},
+		},
+		{
+			name: "none DeviceLink instance",
+			given: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+			},
+			expect: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		var ret = ToDeviceLinkObject(tc.given)
+		if !reflect.DeepEqual(ret, tc.expect) {
+			t.Errorf("case %v: expected %s, got %s", tc.name, spew.Sprintf("%#v", tc.expect), spew.Sprintf("%#v", ret))
+		}
+	}
+}
