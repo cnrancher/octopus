fb68f6f2bc9210fa8fa19cbd32744f3bf3456999
Frank Mai <frank@rancher.com>
2020-05-17 10:36:16 +0800

    refactor(adaptors): print log with device namespacedname

--- template/adaptor/pkg/adaptor/service.go
+++ template/adaptor/pkg/adaptor/service.go
@@ -1,18 +1,34 @@
 package adaptor
 
 import (
+	uberzap "go.uber.org/zap"
+	uberzapcore "go.uber.org/zap/zapcore"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	logr "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	api "github.com/rancher/octopus/pkg/adaptor/api/v1alpha1"
 	"github.com/rancher/octopus/template/adaptor/api/v1alpha1"
 )
 
+var log = logr.NewDelegatingLogger(nil)
+
+func init() {
+	log.Fulfill(zap.New(
+		zap.UseDevMode(true),
+		zap.Level(func() *uberzap.AtomicLevel {
+			level := uberzap.NewAtomicLevelAt(uberzapcore.DebugLevel)
+			return &level
+		}()),
+	))
+}
+
 func NewService() *Service {
 	var scheme = k8sruntime.NewScheme()
-	//
+	// register v1alpha1 scheme into runtime scheme.
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
 
 	return &Service{
