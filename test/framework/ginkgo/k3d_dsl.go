8b35503f63e7a215f495368a430296e3ba1e063b
Huang Wei <775797751@qq.com>
2020-08-14 17:00:20 +0800

    test(mqtt): add e2e cases

--- test/framework/ginkgo/k3d_dsl.go
+++ test/framework/ginkgo/k3d_dsl.go
@@ -18,6 +18,13 @@ func K3dIt(text string, body interface{}, timeout ...float64) bool {
 	return ginkgo.XIt(text, body)
 }
 
+func K3dSpecify(text string, body interface{}, timeout ...float64) bool {
+	if isK3dCluster {
+		return ginkgo.Specify(text, body, timeout...)
+	}
+	return ginkgo.XSpecify(text, body)
+}
+
 func init() {
 	isK3dCluster = !framework.IsUsingExistingCluster() && framework.GetCluster() == cluster.K3d
 }
