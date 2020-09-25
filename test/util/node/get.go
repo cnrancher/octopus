b53290387362775ec1f96ba4ecaa395aa727245f
Frank Mai <frank@rancher.com>
2020-09-25 11:36:45 +0800

    test: support random name k3d testing cluster

--- test/util/node/get.go
+++ test/util/node/get.go
@@ -7,52 +7,32 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/rancher/octopus/test/util/fuzz"
 )
 
 func GetValidWorker(ctx context.Context, k8sCli client.Client) (string, error) {
-	var list = corev1.NodeList{}
-	if err := k8sCli.List(ctx, &list); err != nil {
+	var workers, err = getWorkerSet(ctx, k8sCli)
+	if err != nil {
 		return "", err
 	}
 
-	var workers []string
-	for _, node := range list.Items {
-		if IsOnlyWorker(&node) {
-			workers = append(workers, node.Name)
-		}
-	}
-
-	if len(workers) == 0 {
-		return "", errors.New("no workers")
-	}
-
-	var idx = rand.Intn(len(workers))
-	var name = workers[idx]
+	var idx = rand.Intn(workers.Len())
+	var name = workers.UnsortedList()[idx]
 	return name, nil
 }
 
 func GetInvalidWorker(ctx context.Context, k8sCli client.Client) (string, error) {
-	var list = corev1.NodeList{}
-	if err := k8sCli.List(ctx, &list); err != nil {
+	var workers, err = getWorkerSet(ctx, k8sCli)
+	if err != nil {
 		return "", err
-	}
-
-	var workers = sets.NewString()
-	for _, node := range list.Items {
-		if IsOnlyWorker(&node) {
-			workers.Insert(node.Name)
-		}
-	}
-
-	if workers.Len() == 0 {
-		return "", errors.New("no workers")
 	}
 
 	var invalidWorker string
@@ -63,4 +43,24 @@ func GetInvalidWorker(ctx context.Context, k8sCli client.Client) (string, error)
 		}
 	}
 	return invalidWorker, nil
+}
+
+func getWorkerSet(ctx context.Context, k8sCli client.Client) (sets.String, error) {
+	var workers = sets.NewString()
+	var err = wait.Poll(3*time.Second, 30*time.Second, func() (bool, error) {
+		var list = corev1.NodeList{}
+		if err := k8sCli.List(ctx, &list); err != nil {
+			return false, err
+		}
+		for _, node := range list.Items {
+			if IsOnlyWorker(&node) {
+				workers.Insert(node.Name)
+			}
+		}
+		if workers.Len() == 0 {
+			return false, errors.New("no workers")
+		}
+		return true, nil
+	})
+	return workers, err
 }
